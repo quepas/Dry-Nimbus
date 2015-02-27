435e94163ec6deb85730e531d81da1270dbe4eba
Quepas <oneinchman.dev@gmail.com>
2015-02-27 02:45:00 +0100

    Adding point clouds visualization before/after registration. Cleaning up the code.

--- RegistrationICP_PointCloudLibrary/src/main.cpp
+++ RegistrationICP_PointCloudLibrary/src/main.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
-#include <pcl/io/pcd_io.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
+using pcl::IterativeClosestPoint;
+using pcl::PointCloud;
+using pcl::PointXYZ;
 using pcl::io::loadPLYFile;
+using pcl::visualization::PointCloudColorHandlerCustom;
+using pcl::visualization::PCLVisualizer;
 
-int
- main (int argc, char** argv)
+int main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
+  PointCloud<PointXYZ>::Ptr cloud_base(new PointCloud<PointXYZ>);
+  PointCloud<PointXYZ>::Ptr cloud_target(new PointCloud<PointXYZ>);
+  PointCloud<PointXYZ>::Ptr cloud_base_aligned(new PointCloud<PointXYZ>);
 
-  loadPLYFile("../data/human_1.ply", *cloud_in);
-  loadPLYFile("../data/human_2.ply", *cloud_out);
+  // load point clouds/meshes
+  loadPLYFile("../data/human_1.ply", *cloud_base);
+  loadPLYFile("../data/human_2.ply", *cloud_target);
 
-  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-  icp.setInputCloud(cloud_in);
-  icp.setInputTarget(cloud_out);
-  pcl::PointCloud<pcl::PointXYZ> Final;
-  icp.align(Final);
+  // display point clouds before registration
+  PointCloudColorHandlerCustom<PointXYZ> base_color(cloud_base, 0, 0, 255);
+  PointCloudColorHandlerCustom<PointXYZ> target_color(cloud_target, 255, 0, 0);
+  PCLVisualizer* viewer = new PCLVisualizer("Point clouds BEFORE registration");
+  viewer->addPointCloud(cloud_base, base_color, "cloud_base");
+  viewer->addPointCloud(cloud_target, target_color, "cloud_target");
+  while (!viewer->wasStopped ()) { viewer->spinOnce (100); }
+  viewer->close();
+  delete viewer;
+
+  // register point cloud using iterative closest point algorithm (ICP)
+  std::cout << "ICP started..." << std::endl;
+  IterativeClosestPoint<PointXYZ, PointXYZ> icp;
+  icp.setInputCloud(cloud_base);
+  icp.setInputTarget(cloud_target);
+  icp.align(*cloud_base_aligned);
+
+  // display converged stats
   std::cout << "has converged:" << icp.hasConverged() << " score: " <<
   icp.getFitnessScore() << std::endl;
   std::cout << icp.getFinalTransformation() << std::endl;
+
+  // display point clouds after registration
+  PointCloudColorHandlerCustom<PointXYZ> base_aligned_color (cloud_base_aligned, 255, 0, 0);
+  viewer = new PCLVisualizer("Point clouds AFTER registration");
+  viewer->addPointCloud(cloud_target, target_color, "cloud_target");
+  viewer->addPointCloud(cloud_base_aligned, base_aligned_color, "cloud_base_aligned");
+  while (!viewer->wasStopped ()) { viewer->spinOnce (100); }
 }
