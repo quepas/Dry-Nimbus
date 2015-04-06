89f802405921945de13c0b28be8a5487a0484d35
Quepas <oneinchman.dev@gmail.com>
2015-04-07 01:29:17 +0200

    Loading meshes to compare.

--- PLYMeshDifference_VCGLibrary/src/main.cpp
+++ PLYMeshDifference_VCGLibrary/src/main.cpp
@@ -9,15 +9,26 @@ class CVertex   : public vcg::Vertex<UsedTypes,vcg::vertex::Coord3d,vcg::vertex:
 class CFace     : public vcg::Face< UsedTypes,vcg::face::VertexRef, vcg::face::Normal3d, vcg::face::EdgePlane,vcg::face::Color4b,vcg::face::Mark,vcg::face::BitFlags> {};
 class CMesh     : public vcg::tri::TriMesh< std::vector<CVertex>, std::vector<CFace> > {};
 
+bool LoadMesh(std::string filename, CMesh& mesh) {
+  int errorCode = vcg::tri::io::Importer<CMesh>::Open(mesh, filename.c_str());
+  if(errorCode) {
+    printf("Error in reading %s: '%s'\n", filename, vcg::tri::io::Importer<CMesh>::ErrorMsg(errorCode));
+    if(vcg::tri::io::Importer<CMesh>::ErrorCritical(errorCode)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main()
 {
-  CMesh meshA;
+  CMesh meshA, meshB;
   std::string filenameA = "../data/human_1.ply";
-  // open mesh
-  int errorCode = vcg::tri::io::Importer<CMesh>::Open(meshA, filenameA.c_str());
-  if(errorCode) {
-    printf("Error in reading %s: '%s'\n",filenameA ,vcg::tri::io::Importer<CMesh>::ErrorMsg(errorCode));
-    if(vcg::tri::io::Importer<CMesh>::ErrorCritical(errorCode)) exit(-1);
+  std::string filenameB = "../data/human_2.ply";
+  // open meshes
+  if (!LoadMesh(filenameA, meshA) || !LoadMesh(filenameB, meshB)) {
+    return EXIT_FAILURE;
   }
+  
   std::cout << "Test." << std::endl;
 }
