3d933330ce61db1fc33f44bd0f41625c4e6c13ff
Quepas <oneinchman.dev@gmail.com>
2015-04-07 01:56:06 +0200

    Computing mesh difference.

--- PLYMeshDifference_VCGLibrary/src/main.cpp
+++ PLYMeshDifference_VCGLibrary/src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <wrap/io_trimesh/import.h>
+#include <wrap/io_trimesh/export.h>
+#include <vcg/complex/complex.h>
 #include <vcg/simplex/face/component_ep.h>
+#include <vcg/complex/algorithms/update/component_ep.h>
+#include <vcg/complex/algorithms/update/bounding.h>
+#include "sampling.h"
 
 class CFace;
 class CVertex;
@@ -22,13 +27,43 @@ bool LoadMesh(std::string filename, CMesh& mesh) {
 
 int main()
 {
+  int ColorMin = 0, ColorMax = 0;
   CMesh meshA, meshB;
   std::string filenameA = "../data/human_1.ply";
   std::string filenameB = "../data/human_2.ply";
+
+  int flags = vcg::SamplingFlags::VERTEX_SAMPLING |
+              vcg::SamplingFlags::EDGE_SAMPLING |
+              vcg::SamplingFlags::FACE_SAMPLING |
+              vcg::SamplingFlags::SIMILAR_SAMPLING;
+
   // open meshes
   if (!LoadMesh(filenameA, meshA) || !LoadMesh(filenameB, meshB)) {
     return EXIT_FAILURE;
   }
   
-  std::cout << "Test." << std::endl;
+  int n_samples_target = 10 * max(meshA.fn, meshB.fn);
+
+  vcg::tri::UpdateComponentEP<CMesh>::Set(meshA);
+  vcg::tri::UpdateComponentEP<CMesh>::Set(meshB);
+
+  vcg::tri::UpdateBounding<CMesh>::Box(meshA);
+  vcg::tri::UpdateBounding<CMesh>::Box(meshB);
+
+  // set Bounding Box.
+  vcg::Box3<CMesh::ScalarType> bbox,
+                          tmp_bbox_M1 = meshA.bbox, 
+                          tmp_bbox_M2 = meshB.bbox;
+  bbox.Add(meshA.bbox);
+  bbox.Add(meshB.bbox);
+  bbox.Offset(bbox.Diag() * 0.02);
+  meshA.bbox = bbox;
+  meshB.bbox = bbox;
+
+  vcg::Sampling<CMesh> ForwardSampling(meshA, meshB);
+  ForwardSampling.SetFlags(flags);
+  ForwardSampling.SetSamplesTarget(n_samples_target);
+  ForwardSampling.Hausdorff();
+
+  std::cout << "Test: " << ForwardSampling.GetDistMax() << std::endl;
 }
