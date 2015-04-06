b6c1a1fd86793c74c6d0b1b7ef96c0a7bd49c126
Quepas <oneinchman.dev@gmail.com>
2015-04-07 00:23:42 +0200

    Adding initial geometry definitions and loading of ply mesh.

--- PLYMeshDifference_VCGLibrary/src/main.cpp
+++ PLYMeshDifference_VCGLibrary/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <wrap/io_trimesh/import.h>
+#include <vcg/simplex/face/component_ep.h>
+
+class CFace;
+class CVertex;
+struct UsedTypes:public vcg::UsedTypes< vcg::Use<CFace>::AsFaceType, vcg::Use<CVertex>::AsVertexType>{};
+class CVertex   : public vcg::Vertex<UsedTypes,vcg::vertex::Coord3d,vcg::vertex::Qualityf,vcg::vertex::Normal3d,vcg::vertex::Color4b,vcg::vertex::BitFlags> {};
+class CFace     : public vcg::Face< UsedTypes,vcg::face::VertexRef, vcg::face::Normal3d, vcg::face::EdgePlane,vcg::face::Color4b,vcg::face::Mark,vcg::face::BitFlags> {};
+class CMesh     : public vcg::tri::TriMesh< std::vector<CVertex>, std::vector<CFace> > {};
 
 int main()
 {
+  CMesh meshA;
+  std::string filenameA = "../data/human_1.ply";
+  // open mesh
+  int errorCode = vcg::tri::io::Importer<CMesh>::Open(meshA, filenameA.c_str());
+  if(errorCode) {
+    printf("Error in reading %s: '%s'\n",filenameA ,vcg::tri::io::Importer<CMesh>::ErrorMsg(errorCode));
+    if(vcg::tri::io::Importer<CMesh>::ErrorCritical(errorCode)) exit(-1);
+  }
   std::cout << "Test." << std::endl;
 }
